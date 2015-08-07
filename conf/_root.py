e59627b0de121b9f7782059d8e5de15e3723c3c9
rainedream <raineddream@gmail.com>
2015-08-07 10:44:09 +0800

    Find project root that is parent folder of 'src'

--- conf/_root.py
+++ conf/_root.py
@@ -1,7 +1,26 @@
 # coding=utf-8
 import os
-from os.path import dirname, abspath
+from os.path import dirname, abspath, isdir
 
 
 def project_root():
-    return os.getenv('PROJ_ROOT', abspath(dirname((dirname(dirname(__file__))))))
+    srcpath = _find_src_dir(__file__)
+    return os.getenv('PROJ_ROOT', abspath(dirname(srcpath)))
+
+
+def _find_src_dir(filepath):
+    dirpath = dirname(filepath)
+    if dirpath == '/':
+        raise OSError("Cannot find 'src' folder.")
+
+    if isdir(dirpath) and _folder_name(dirpath) == 'src':
+        return dirpath
+
+    return _find_src_dir(dirpath)
+
+
+def _folder_name(dirpath):
+    index = dirpath.rfind('/')
+    if index < 0:
+        return ''
+    return dirpath[index + 1:]
