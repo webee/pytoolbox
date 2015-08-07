34519c62281f712b9aded49e6b99da1ac7619aeb
rainedream <raineddream@gmail.com>
2015-08-07 10:18:23 +0800

    Can get project root with tool

--- conf/__init__.py
+++ conf/__init__.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-def load(config_package, env='dev'):
+def load(config_package, env):
     from _config_loader import load as _load
     return _load(config_package, env=env)
 
@@ -8,3 +8,8 @@ def load(config_package, env='dev'):
 def read_file(filepath):
     from _reader import read_file as _read_file
     return _read_file(filepath)
+
+
+def project_root():
+    from _root import project_root as _project_root
+    return _project_root()
