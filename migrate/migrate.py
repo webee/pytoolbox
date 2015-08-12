a134c4d927b021dc400a22bc79adec7b19921440
liufan <liufan@lvye.com>
2015-08-12 16:25:33 +0800

    refactor on migration logic

--- migrate/migrate.py
+++ migrate/migrate.py
@@ -5,13 +5,45 @@ import glob
 import os
 from ..util.dbe import from_db
 
+MIGRATION_RECORD_TABLE = 'migrate_command_center'
 
 def migrate(script_path):
-    migrate_table = 'migrate_command_center'
-    print('Begin to migrate ...')
-    print("Migrate table is '%s'" % migrate_table)
+    try_create_migration_table()
 
-    try:
+    clear_previous_failed_script_record()
+
+    executed = [rec.script_name for rec in from_db().list('SELECT script_name FROM %s' % migrate_table)]
+    scripts = [script for script in glob.glob(os.path.join(script_path, '*.*'))]
+    scripts.sort()
+    for script in scripts:
+        script_name = script[script.rfind('/') + 1:]
+        if is_valid_migration_script(script_name) and script_name not in executed:
+            try:
+                print('[MIGRATE] - execute: %s' % script_name)
+                from_db().execute(open(script, 'r').read())
+            except Exception:
+                print('Error to execute script %s' % script_name)
+                from_db().execute('INSERT INTO ' + migrate_table + '(script_name, success) VALUES(%s, FALSE)', (script_name,))
+                raise
+            from_db().execute('INSERT INTO ' + migrate_table + '(script_name) VALUES(%s)', (script_name,))
+
+    print('Database is up to date')
+
+
+def is_valid_migration_script(script_name):
+    return script_name[0:3].isdigit() and script_name.endswith('.sql')
+
+
+def clear_previous_failed_script_record():
+    error_script = from_db().get_scalar('SELECT script_name FROM %s WHERE success = FALSE' % migrate_table)
+    if error_script:
+        print("There is a failed script in last run: %s \nPlease make sure it is fixed." % error_script)
+        from_db().execute('delete from ' + migrate_table + ' where success = False')
+
+
+def try_create_migration_table():
+    migrate_table_in_db = from_db().get("show tables like '{}' ".format(migrate_table))
+    if not migrate_table_in_db:
         from_db().execute(
             """
             CREATE TABLE %s(
@@ -20,37 +52,8 @@ def migrate(script_path):
                 executed_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
             )
             """ % migrate_table)
-    except Exception as err:
-        if 'already exists' not in str(err):
-            raise
-
-    error_script = from_db().get_scalar('SELECT script_name FROM %s WHERE success = FALSE' % migrate_table)
-    if error_script:
-        print("There is a failed script in last run: %s \nPlease make sure it is fixed." % error_script)
-        from_db().execute('delete from ' + migrate_table + ' where success = False')
-
-    executed = [rec.script_name for rec in from_db().list('SELECT script_name FROM %s' % migrate_table)]
-    scripts = [script for script in glob.glob(os.path.join(script_path, '*.*'))]
-    scripts.sort()
-    for script in scripts:
-        script_name = script[script.rfind('/') + 1:]
-        if script_name not in executed:
-            try:
-                print('[MIGRATE] - execute: %s' % script_name)
-                if script_name.endswith('.sql'):
-                    from_db().execute(open(script, 'r').read())
-                else:
-                    raise Exception('Unsupported migrate script: %s' % script_name)
-            except Exception:
-                print('Error to execute script %s' % script_name)
-                from_db().execute('INSERT INTO ' + migrate_table + '(script_name, success) VALUES(%s, FALSE)',
-                                  (script_name,))
-                raise
-            from_db().execute('INSERT INTO ' + migrate_table + '(script_name) VALUES(%s)', (script_name,))
-
-    print('System is up to date')
 
 
 if __name__ == '__main__':
-    migrate('migrate/schema')
+    migrate('migration')
 
