62cfedb244b21ce3731c240fbb03e410adaa9a37
liufan <liufan@lvye.com>
2015-08-12 16:44:28 +0800

    fix build

--- migrate/migrate.py
+++ migrate/migrate.py
@@ -12,7 +12,7 @@ def migrate(script_path):
 
     clear_previous_failed_script_record()
 
-    executed = [rec.script_name for rec in from_db().list('SELECT script_name FROM %s' % migrate_table)]
+    executed = [rec.script_name for rec in from_db().list('SELECT script_name FROM %s' % MIGRATION_RECORD_TABLE)]
     scripts = [script for script in glob.glob(os.path.join(script_path, '*.*'))]
     scripts.sort()
     for script in scripts:
@@ -23,9 +23,9 @@ def migrate(script_path):
                 from_db().execute(open(script, 'r').read())
             except Exception:
                 print('Error to execute script %s' % script_name)
-                from_db().execute('INSERT INTO ' + migrate_table + '(script_name, success) VALUES(%s, FALSE)', (script_name,))
+                from_db().insert(MIGRATION_RECORD_TABLE, script_name=script_name, success=False)
                 raise
-            from_db().execute('INSERT INTO ' + migrate_table + '(script_name) VALUES(%s)', (script_name,))
+            from_db().insert(MIGRATION_RECORD_TABLE, script_name=script_name)
 
     print('Database is up to date')
 
@@ -35,14 +35,14 @@ def is_valid_migration_script(script_name):
 
 
 def clear_previous_failed_script_record():
-    error_script = from_db().get_scalar('SELECT script_name FROM %s WHERE success = FALSE' % migrate_table)
+    error_script = from_db().get_scalar('SELECT script_name FROM %s WHERE success = FALSE' % MIGRATION_RECORD_TABLE)
     if error_script:
         print("There is a failed script in last run: %s \nPlease make sure it is fixed." % error_script)
-        from_db().execute('delete from ' + migrate_table + ' where success = False')
+        from_db().execute('delete from ' + MIGRATION_RECORD_TABLE + ' where success = False')
 
 
 def try_create_migration_table():
-    migrate_table_in_db = from_db().get("show tables like '{}' ".format(migrate_table))
+    migrate_table_in_db = from_db().get("show tables like '{}' ".format(MIGRATION_RECORD_TABLE))
     if not migrate_table_in_db:
         from_db().execute(
             """
@@ -51,7 +51,7 @@ def try_create_migration_table():
                 success BOOLEAN NOT NULL DEFAULT TRUE,
                 executed_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
             )
-            """ % migrate_table)
+            """ % MIGRATION_RECORD_TABLE)
 
 
 if __name__ == '__main__':
