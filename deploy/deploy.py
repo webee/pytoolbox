3ea3ab46ea4625a3ed9f878ec73763f3e93bc5a6
rainedream <raineddream@gmail.com>
2015-08-26 18:38:30 +0800

    Deployment tool

---
+++ deploy/deploy.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, division
+from fabric import api as fab
+from fabric.contrib.files import exists, upload_template
+from os import path, environ
+from util.attr_dict import AttrDict
+from util.root import root_path
+
+
+class Deployment(object):
+    def __init__(self, git_repo, project_dir, server_name, site_name, supervisord_config_path, context, supervisord_remote_dir):
+        self._git_repo = git_repo
+        self._project_dir = project_dir
+        self._server_name = server_name
+        self._site_name = site_name
+        self._supervisord = AttrDict(config_path=supervisord_config_path, context=context,
+                                     remote_dir=supervisord_remote_dir)
+
+    def do(self, server_host, user, pip_install=False, bower_install=False, db_migration=False):
+        fab.use_ssh_config = True
+        fab.env.host_string = '{0}@{1}'.format(user, server_host)
+        fab.env.user = user
+
+        self._update_codes()
+        self._update_supervisord_config()
+
+        self._stop_api_server(self._server_name)
+        if pip_install:
+            self._pip_install()
+        if bower_install:
+            self._bower_install()
+        if db_migration:
+            self._migrate_db()
+        self._start_api_server(self._server_name)
+
+    def _update_codes(self):
+        repo_dir = self._project_dir
+
+        if not exists(repo_dir):
+            self._clone_codes(self._git_repo, repo_dir)
+        else:
+            self._pull_codes(repo_dir)
+
+    def _clone_codes(self, git_repo, base_dir):
+        fab.run('git clone --recursive {0} {1}'.format(git_repo, base_dir))
+        self._create_venv(base_dir)
+
+    def _create_venv(self, base_dir):
+        with fab.cd(base_dir):
+            fab.run('virtualenv venv')
+
+    def _pull_codes(self, base_dir):
+        with fab.cd(base_dir):
+            fab.run('git pull --ff-only origin master')
+        with fab.cd(path.join(base_dir, 'libraries/pytoolbox')):
+            fab.run('git pull --ff-only origin master')
+
+    def _update_supervisord_config(self):
+        self._upload_supervisord_conf(self._supervisord.config_path)
+        fab.run('sudo /usr/local/bin/supervisorctl reread')
+        fab.run('sudo /usr/local/bin/supervisorctl update')
+
+    def _upload_supervisord_conf(self, template_relative_path):
+        root = root_path(self._site_name)
+        template_file_path = path.join(root, template_relative_path)
+        template_file_name = path.basename(template_file_path)
+        template_dir = path.dirname(template_file_path)
+        remote_path = '{0}/{1}.conf'.format(self._supervisord.remote_dir, self._server_name)
+
+        upload_template(template_file_name, remote_path, context=self._supervisord.context, use_jinja=True, template_dir=template_dir,
+                        use_sudo=True, backup=False)
+
+    def _stop_api_server(self, name):
+        fab.run('sudo /usr/local/bin/supervisorctl stop {}'.format(name))
+
+    def _start_api_server(self, name):
+        fab.run('sudo /usr/local/bin/supervisorctl start {}'.format(name))
+
+    def _migrate_db(self):
+        with fab.cd(self._site_dir()), fab.prefix('source venv/bin/activate'):
+            fab.run('python src/manager.py -e {0} migrate'.format(environ['ENV']))
+
+    def _pip_install(self):
+        with fab.cd(self._site_dir()), fab.prefix('source venv/bin/activate'):
+            fab.run('pip install -r requirements.txt'.format(environ['ENV']))
+
+    def _bower_install(self):
+        with fab.cd(self._site_dir()):
+            fab.run('bower install')
+
+    def _site_dir(self):
+        return path.join(self._project_dir, self._site_name)
