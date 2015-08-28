f71bdb5dc80cb5d3b332de68bb24ef3c6c92fe95
webee <webee.yw@gmail.com>
2015-08-28 14:00:28 +0800

    update pay_client.

--- pay_client/__init__.py
+++ pay_client/__init__.py
@@ -1,9 +1,11 @@
 # coding=utf-8
+from __future__ import unicode_literals
 from functools import wraps
 from decimal import Decimal
 
 import os
 import requests
+from collections import namedtuple
 from ..util.sign import SignType, Signer
 from ..util import pmc_config
 from ..util.log import get_logger
@@ -12,6 +14,17 @@ from .config import Config
 
 
 logger = get_logger(__name__)
+Result = namedtuple('Result', 'status_code, data')
+
+
+def _is_success_result(result):
+    if result is None:
+        return False
+    ret = result.data['ret']
+    if not ret:
+        logger.warn("failed result: code: {0}, msg: {1}, data: {2}".format(result.data['code'],
+                                                                           result.data['msg'], result.data))
+    return ret
 
 
 class PayClient(object):
@@ -51,6 +64,10 @@ class PayClient(object):
             return f(*args, **kwargs)
         return wrapper
 
+    def _generate_api_url(self, url, **kwargs):
+        url = url.lstrip('/')
+        return os.path.join(self.config.ROOT_URL, url.format(**kwargs))
+
     def request(self, url, params=None, sign_type=SignType.RSA, method='post'):
         if params is None:
             params = {}
@@ -60,64 +77,85 @@ class PayClient(object):
         params['sign'] = self.signer.sign(params, sign_type)
 
         try:
+            logger.info("request {0} {1}: {2}".format(method, url, params))
             req = requests.request(method, url, data=params)
             try:
                 if req.status_code != 200:
-                    logger.warn('bad request result: [{0}]'.format(req.text))
-                    return None
-                return req.json()
+                    logger.warn('failed request result: [{0}], [{1}]'.format(req.status_code, req.text))
+                logger.info('request result: [{0}], [{1}]'.format(req.status_code, req.text))
+                return Result(req.status_code, req.json())
             except Exception as e:
                 logger.exception(e)
-                logger.warn('bad request: {0}, {1}'.format(req.status_code, req.text))
+                return None
         except Exception as e:
             logger.exception(e)
         return None
 
-    def _generate_api_url(self, url, **kwargs):
-        url = url.lstrip('/')
-        url = os.path.join(self.config.ROOT_URL, url.format(**kwargs))
-        logger.info("request url %s" % url)
-        return url
+    def get_req(self, url, params=None):
+        return self.request(url, params, method='get')
 
-    def get_account_user_id(self, user_id, user_domain_name=None):
+    def post_req(self, url, params=None):
+        return self.request(url, params, method='post')
+
+    def query_user_is_opened(self, user_id):
+        params = {
+            'user_id': user_id
+        }
+
+        url = self._generate_api_url(self.config.QUERY_USER_IS_OPENED_URL, **params)
+
+        result = self.get_req(url, params)
+        if _is_success_result(result):
+            return result.data['is_opened']
+        return False
+
+    def get_account_user(self, user_id):
         if user_id not in self._uid_accounts:
-            user_domain_name = user_domain_name or self.config.USER_DOMAIN_NAME
-            url = self._generate_api_url(self.config.GET_CREATE_ACCOUNT_ID_URL,
-                                         user_domain_name=user_domain_name, user_id=user_id)
-            data = self.request(url)
-            if data is not None:
-                self._uid_accounts[user_id] = data['account_user_id']
+            params = {
+                'user_id': user_id
+            }
+            url = self._generate_api_url(self.config.GET_ACCOUNT_USER_URL, **params)
+            result = self.get_req(url, params)
+            if _is_success_result(result):
+                self._uid_accounts[user_id] = result.data['account_user_id']
         return self._uid_accounts.get(user_id)
 
-    def get_user_balance(self, user_id):
-        account_user_id = self.get_account_user_id(user_id)
-        url = self._generate_api_url(self.config.GET_USER_BALANCE_URL, account_user_id=account_user_id)
+    def get_create_account_user(self, user_id):
+        if user_id not in self._uid_accounts:
+            params = {
+                'user_id': user_id
+            }
+            url = self._generate_api_url(self.config.GET_CREATE_ACCOUNT_USER_URL, **params)
+            result = self.post_req(url, params)
+            if _is_success_result(result):
+                self._uid_accounts[user_id] = result.data['account_user_id']
+        return self._uid_accounts.get(user_id)
 
-        data = self.request(url, method='get')
+    def query_user_balance(self, user_id):
+        account_user_id = self.get_account_user(user_id)
+        if account_user_id is None:
+            return {'total': Decimal(0), 'available': Decimal(0), 'frozen': Decimal(0)}
 
-        if data is not None:
-            return data['data']
+        params = {
+            'account_user_id': account_user_id
+        }
+        url = self._generate_api_url(self.config.QUERY_USER_BALANCE_URL, **params)
+        result = self.get_req(url, params)
+
+        if _is_success_result(result):
+            return result.data['data']
         return {'total': Decimal(0), 'available': Decimal(0), 'frozen': Decimal(0)}
 
+    def query_user_available_balance(self, uid):
+        return self.query_user_balance(uid)['available']
+
     def prepay(self, params):
+        params = dict(params)
         url = self._generate_api_url(self.config.PREPAY_URL)
-        return self.request(url, params)
-
-    def refund(self, order_id=None, amount=None, notify_url=None, params=None):
-        if params is None:
-            params = {
-                'order_id': order_id,
-                'amount': amount,
-                'notify_url': notify_url
-            }
-
-        url = self._generate_api_url(self.config.REFUND_URL)
-        return self.request(url, params)
-
-    def withdraw(self, user_id, params):
-        account_user_id = self.get_account_user_id(user_id)
-        url = self._generate_api_url(self.config.WITHDRAW_URL, account_user_id=account_user_id)
-        return self.request(url, params)
+        result = self.post_req(url, params)
+        if _is_success_result(result):
+            return result.data['pay_url']
+        return None
 
     def confirm_guarantee_payment(self, order_id):
         params = {
@@ -125,46 +163,99 @@ class PayClient(object):
         }
 
         url = self._generate_api_url(self.config.CONFIRM_GUARANTEE_PAYMENT_URL)
-        return self.request(url, params)
+        result = self.post_req(url, params)
 
-    def get_user_available_balance(self, uid):
-        return self.get_user_balance(uid)[1]
+        return _is_success_result(result)
+
+    def refund(self, order_id=None, amount=None, notify_url=None, params=None, ret_result=False):
+        if params is None:
+            params = {
+                'order_id': order_id,
+                'amount': amount,
+                'notify_url': notify_url
+            }
+        else:
+            params = dict(params)
+
+        url = self._generate_api_url(self.config.REFUND_URL)
+        result = self.post_req(url, params)
+        if ret_result:
+            return result
+
+        if _is_success_result(result):
+            return result.data['sn']
+        return None
+
+    def withdraw(self, user_id, bankcard_id=None, amount=None, notify_url=None, params=None, ret_result=False):
+        if params is None:
+            params = {
+                'bankcard_id': bankcard_id,
+                'amount': amount,
+                'notify_url': notify_url
+            }
+        else:
+            params = dict(params)
+        account_user_id = self.get_account_user_id(user_id)
+        params['from_user_id'] = account_user_id
+
+        url = self._generate_api_url(self.config.WITHDRAW_URL)
+        result = self.post_req(url, params)
+        if ret_result:
+            return result
+
+        if _is_success_result(result):
+            return result.data['sn']
+        return None
 
     def list_user_bankcards(self, uid):
-        account_user_id = self.get_account_user_id(uid)
-        url = self._generate_api_url(self.config.LIST_USER_BANKCARDS_URL, account_user_id=account_user_id)
-        data = self.request(url, method='get')
+        account_user_id = self.get_account_user(uid)
+        if account_user_id is None:
+            return []
 
-        if data is not None:
-            return data['bankcards']
+        params = {
+            'account_user_id': account_user_id
+        }
+        url = self._generate_api_url(self.config.LIST_USER_BANKCARDS_URL, **params)
+
+        result = self.get_req(url, params)
+        if _is_success_result(result):
+            return result.data['bankcards']
         return []
 
-    def get_bin(self, card_no):
-        url = self._generate_api_url(self.config.QUERY_BIN_URL, card_no=card_no)
-
-        return self.request(url, method='get')
-
-    def query_user_is_opened(self, user_id):
+    def query_bin(self, card_no):
         params = {
-            'user_domain_name': self.config.USER_DOMAIN_NAME,
-            'user_id': user_id
+            'card_no': card_no
         }
+        url = self._generate_api_url(self.config.QUERY_BIN_URL, **params)
 
-        url = self._generate_api_url(self.config.QUERY_USER_IS_OPENED_URL, **params)
-
-        return self.request(url, params, method='get')
+        result = self.get_req(url, params)
+        if _is_success_result(result):
+            return result.data['data']
+        return None
 
     def query_transactions(self, uid, role, page_no, page_size, q):
         account_user_id = self.get_account_user_id(uid)
-        url = self._generate_api_url(self.config.GET_USER_TRANSACTIONS_URL, account_user_id=account_user_id)
+        if account_user_id is None:
+            return None
 
         params = {
+            'account_user_id': account_user_id
+        }
+        url = self._generate_api_url(self.config.GET_USER_TRANSACTIONS_URL, **params)
+
+        q_params = {
             'role': role,
             'page_no': page_no,
             'page_size': page_size
         }
         if q:
-            params['q'] = q
+            q_params['q'] = q
+
+        params.update(q_params)
 
         url = build_url(url, **params)
-        return self.request(url, method='get')
+        result = self.get_req(url, params)
+
+        if _is_success_result(result):
+            return result.data['data']
+        return None
--- pay_client/config.py
+++ pay_client/config.py
@@ -5,22 +5,23 @@ class Config:
     MD5_KEY = ''
     LVYE_PUB_KEY = ''
     CHANNEL_PRI_KEY = ''
-    # 测试用户
-    USER_DOMAIN_NAME = 'testing'
     # sample
     CHANNEL_NAME = 'zyt_sample'
 
     ROOT_URL = "http://pay.lvye.com/api/__"
-    QUERY_USER_IS_OPENED_URL = "/user_mapping/user_domains/{user_domain_name}/users/{user_id}/is_opened"
+    QUERY_USER_IS_OPENED_URL = "/user_mapping/users/{user_id}/is_opened"
     PREPAY_URL = '/biz/prepay'
     CONFIRM_GUARANTEE_PAYMENT_URL = '/biz/pay/guarantee_payment/confirm'
     REFUND_URL = '/biz/refund'
-    WITHDRAW_URL = '/application/account_users/{account_user_id}/withdraw'
+    WITHDRAW_URL = '/biz/withdraw'
+    APP_WITHDRAW_URL = '/application/withdraw'
     PREPAID_URL = '/biz/prepaid'
 
-    GET_CREATE_ACCOUNT_ID_URL = '/user_mapping/user_domains/{user_domain_name}/users/{user_id}'
-    GET_USER_BALANCE_URL = "/vas/zyt/account_users/{account_user_id}/balance"
-    LIST_USER_BANKCARDS_URL = 'application/account_users/{account_user_id}/bankcards'
+    GET_ACCOUNT_USER_URL = '/user_mapping/users/{user_id}'
+    GET_CREATE_ACCOUNT_USER_URL = '/user_mapping/users/{user_id}'
 
-    GET_USER_TRANSACTIONS_URL = "biz/account_users/{account_user_id}/transactions"
-    QUERY_BIN_URL = 'application/bankcard/{card_no}/bin'
+    QUERY_BIN_URL = '/application/bankcard/{card_no}/bin'
+    QUERY_USER_BALANCE_URL = "/vas/zyt/account_users/{account_user_id}/balance"
+    LIST_USER_BANKCARDS_URL = '/application/account_users/{account_user_id}/bankcards'
+
+    GET_USER_TRANSACTIONS_URL = "/biz/account_users/{account_user_id}/transactions"
