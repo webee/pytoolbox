ec82be90d3342e32d6f649ff568019e8b7e4a87c
webee <webee.yw@gmail.com>
2015-09-21 03:05:58 +0800

    add constant for pay_client.

---
+++ pay_client/constant.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+
+class VirtualAccountSystemType:
+    ZYT = 'ZYT'
+    TEST_PAY = 'TEST_PAY'
+    LIANLIAN_PAY = 'LIANLIAN_PAY'
+    WEIXIN_PAY = 'WEIXIN_PAY'
+    ALI_PAY = 'ALI_PAY'
+
+
+class TransactionType:
+    PREPAID = 'PREPAID'
+    PAYMENT = 'PAYMENT'
+    TRANSFER = 'TRANSFER'
+    REFUND = 'REFUND'
+    WITHDRAW = 'WITHDRAW'
+
+
+class RequestClientType:
+    WEB = 'WEB'
+    WAP = 'WAP'
+    IOS = 'IOS'
+    ANDROID = 'ANDROID'
