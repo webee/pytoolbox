23ed97c35fddbd05332878cc300afee0e773644c
易旺 <yiwang@lvye.com>
2015-11-19 22:19:45 +0800

    update strings.

--- util/strings.py
+++ util/strings.py
@@ -4,4 +4,6 @@ import random
 
 
 def gen_rand_str(size=32, chars=string.ascii_uppercase + string.digits):
+    if size <= 0:
+        return ''
     return ''.join(random.choice(chars) for _ in range(size))
