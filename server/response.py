c5cd9972a4e38e7e86d00f10c5b18e29faa13712
rainedream <raineddream@gmail.com>
2015-12-11 12:06:24 +0800

    Add forbidden into response methods

--- server/response.py
+++ server/response.py
@@ -34,6 +34,10 @@ def no_content():
     return _response(204)
 
 
+def forbidden():
+    return _response(403)
+
+
 def not_found(params=None):
     return _response(404, {'error': 'not found', 'params': params})
 
